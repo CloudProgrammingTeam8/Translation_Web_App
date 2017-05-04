431bdbcea026daaaecbf4eebc9bf55c6149aa383
ashley <ashley830204@gmail.com>
2017-05-04 11:44:46 +0800

    eb version

---
+++ Translation_web_app/settings_secret.py
@@ -0,0 +1,6 @@
+Engine = 'django.db.backends.mysql'
+DBname = 'DjangoRDS'
+Username = 'bossi'
+Password = 'bossiwai830601'
+Endpoint = 'django.cldbugjrni6b.us-east-1.rds.amazonaws.com'
+Port ='3306'
